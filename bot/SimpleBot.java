3f2d5053e6de057c573888fa6097fa1b400f8b76
ashu <ashutosh.kuma_cs18>
2020-04-10 11:29:41 +0530

    initial commit..

---
+++ bot/SimpleBot.java
@@ -0,0 +1,10 @@
+package bot;
+
+/**
+ * This is simple bot class where we show the name of assistant,take the name of user,
+ * calculate user age,throe the question in front of the user.
+ */
+
+public class SimpleBot {
+
+}
