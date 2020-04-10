2d7569addc195b4df96d01b9fb3ba3f5490d5d85
ashu <ashutosh.kuma_cs18>
2020-04-10 11:31:00 +0530

    add the object of Scanner class i.e. sc...

--- bot/SimpleBot.java
+++ bot/SimpleBot.java
@@ -1,10 +1,13 @@
 package bot;
 
+import java.util.Scanner;
+
 /**
  * This is simple bot class where we show the name of assistant,take the name of user,
  * calculate user age,throe the question in front of the user.
  */
 
 public class SimpleBot {
+    final Scanner sc = new Scanner(System.in);
 
 }
