92798ee505dd572cac0bc21486cb42705421683e
ashu <ashutosh.kuma_cs18>
2020-04-10 12:05:28 +0530

    add variable answer of type integer...for asking the answer..

--- bot/SimpleBot.java
+++ bot/SimpleBot.java
@@ -63,5 +63,14 @@ public class SimpleBot {
     }
 
     static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods?");
+        while (true) {
+            System.out.println("1. To repeat a statement multiple times.");
+            System.out.println("2. To decompose a program into several small subroutines.");
+            System.out.println("3. To determine the execution time of a program.");
+            System.out.println("4. To interrupt the execution of a program.");
+            int answer = sc.nextInt();
+        }
     }
 }
